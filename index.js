c56524a8446a470e123bfcd54ca26fded0e2a2ba
marion <kmarion548@gmail.com>
2021-10-16 19:17:14 +0300

    change styels

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ img.src = "lake.jpg";
 
 let para = document.createElement("p");
 para.textContent =
-  "People must be front and centre in any New Deal, with everyone entitled to live in a healthy environment and the human rights of local communities and indigenous peoples respected. A New Deal can only be achieved in partnership with, and in the interests of, the people who share their lands with nature.";
+  "People must be front and centre in any New Deal, with everyone entitled to live in a healthy environment and the human rights of local communities and indigenous peoples respected. A New Deal can only be achieved in partnership with, and in the interests.";
 firstDiv.appendChild(heading);
 firstDiv.appendChild(img);
 firstDiv.appendChild(para);
 container.appendChild(firstDiv);
 
 let newDiv = document.createElement("div");
-newDiv.className = "div2";
+newDiv.className = "div";
 
 let heading2 = document.createElement("h3");
 heading2.textContent = "i love nature";
